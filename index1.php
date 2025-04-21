19a046447f26bb7b72bc80a4531c3d70e6297f6e
Blessing Lartey <bbklartey@gmail.com>
2025-04-21 20:29:50 +0000

    php hands on

---
+++ index1.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+<body>
+        <?php
+            $name = "Dark Matter";
+            $read = true;
+            $books = [
+                [
+                    'name' => 'book1',
+                    'author' => 'someone1',
+                    'price' => 40
+                ],
+                [
+                    'name' => 'book2',
+                    'author' => 'someone2',
+                    'price' => 45
+                ]
+            ];
+
+            function filterByAuthor($books, $author) {
+                $filteredBooks = [];
+
+                foreach ($books as $book) {
+                    if ($book['author'] === $author) {
+
+                        $filteredBooks[] = $book;
+                    }
+                }
+                return $filteredBooks;
+            }
+
+            if ($read) {
+                $message = "you have read $name";
+            } else {
+                $message = "you have not read $name";
+            }
+        ?>
+
+        <h1>Recommended Books</h1>
+
+    <h1>
+        
+        <?= $message ?>
+    </h1>
+   
+
+    <ul>
+
+        <!-- looping over an array -->
+        <?php foreach ($books as $book):
+        ?>
+
+
+            <!-- Filtering through the array -->
+            <?php if ($book['author'] === 'someone2') :  ?>
+
+        <li><?= $book['name']; ?></li>
+        <?php endif; ?>
+        <?php endforeach; ?>
+
+        <!-- getting an object in an array -->
+         <!-- <?= "$books[1] is the second book on the list"?>  -->
+    </ul>
+
+    <ul>
+    <!-- Filtering through the array using function -->
+    <?php foreach (filterByAuthor($books, 'someone1') as $book);?>
+
+<li><?="this book:" . $book['name']; ?></li>
+
+
+<!-- getting an object in an array -->
+ <!-- <?= "$books[1] is the second book on the list"?>  -->
+</ul>
+
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,16 @@
+<?php
+
+$business = [
+    'name' => 'Seambybee',
+    'cost' => 45,
+    'categories' => ["bubu", "corset", "tops"]
+];
+
+function register($user) {
+    //create the user record in the db
+    //log them in
+    //Send a welcome email
+    //redirect to their dashboard
+}
+
+require "index2.view.php";
